94686e12f175719041671a377e34a537b60e1303
pinky-pig <2545589993@qq.com>
2023-11-10 16:14:40 +0800

    feat: add indexDB

--- src/logic/storage.ts
+++ src/logic/storage.ts
@@ -1,5 +1,8 @@
+import { useStorageIndexDB } from '~/composables/useStorageIndexDB'
 import { useStorageLocal } from '~/composables/useStorageLocal'
 
 export const storageDemo = useStorageLocal('webext-demo', 'Storage Demo')
 
 export const isShrinkSidebar = useStorageLocal('isShrinkSidebar', 'false')
+
+export const storageWebsiteDB = useStorageIndexDB('website_db', '++id, webName, url, icon, type') // 0 pined 1 most used
---
+++ src/logic/websiteData.ts
@@ -0,0 +1,29 @@
+import { storageWebsiteDB } from '.'
+
+export interface WebsiteParams {
+  id?: number
+  url: string
+  type: number
+  webName: string
+  icon: string
+  index: number
+  property: { color: string }
+}
+export async function getPinedWebsite() {
+  const queryResult = await storageWebsiteDB.getItemBySQL(
+    { key: 'where', value: 'type' },
+    { key: 'equals', value: 0 },
+    { key: 'toArray', value: null },
+  )
+
+  return queryResult
+}
+export async function addPinedWebsite(params: WebsiteParams) {
+  return await storageWebsiteDB.addItem(params)
+}
+export async function deletePinedWebsite(params: number) {
+  return await storageWebsiteDB.removeItem(params)
+}
+export async function editPinedWebsite(params: WebsiteParams) {
+  return await storageWebsiteDB.editItem(params)
+}
