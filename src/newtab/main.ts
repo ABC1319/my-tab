14219fa78c0491436d96c07bfd7b781882db6f00
pinky-pig <2545589993@qq.com>
2023-11-14 11:52:54 +0800

    feat: add daisyui

--- src/newtab/main.ts
+++ src/newtab/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import { setupApp } from '~/logic/common-setup'
-
+import 'daisyui/dist/full.css'
 import '../styles'
 
 const app = createApp(App)
