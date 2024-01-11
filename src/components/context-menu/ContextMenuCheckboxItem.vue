6a7924f62980767e020c4531cab9ad5615633365
pinky-pig <2545589993@qq.com>
2024-01-11 11:07:32 +0800

    fix: contextMenu 关闭的动画时间

--- src/components/context-menu/ContextMenuCheckboxItem.vue
+++ src/components/context-menu/ContextMenuCheckboxItem.vue
@@ -6,8 +6,6 @@ import {
   ContextMenuItemIndicator,
   useEmitAsProps,
 } from 'radix-vue'
-import { Check } from 'lucide-vue-next'
-import { cn } from '@/lib/utils'
 
 const props = defineProps<ContextMenuCheckboxItemProps & { class?: string }>()
 const emits = defineEmits<ContextMenuCheckboxItemEmits>()
@@ -16,18 +14,14 @@ const emits = defineEmits<ContextMenuCheckboxItemEmits>()
 <template>
   <ContextMenuCheckboxItem
     v-bind="{ ...props, ...useEmitAsProps(emits) }"
-    :class="[
-      cn(
-        'relative flex cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50',
-        props.class,
-      ),
-    ]"
+    class="relative flex cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50"
+    :class="props.class"
   >
     <span class="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
       <ContextMenuItemIndicator
         class="absolute left-1.5 inline-flex w-4 h-4 items-center justify-center"
       >
-        <Check class="h-4 h-w" />
+        <svg class="h-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 6L9 17l-5-5" /></svg>
       </ContextMenuItemIndicator>
     </span>
     <slot />
--- unocss.config.ts
+++ unocss.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   shortcuts: [
     {
       'animate-in': 'animate-[enter_0.15s]',
-      'animate-out': 'animate-[out_0.15s]',
+      'animate-out': 'animate-[out_0.01s]',
     },
     [/^btn-(\w+)$/, ([_, color]) => `op50 px2.5 py1 transition-all duration-200 ease-out no-underline! hover:(op100 text-${color} bg-${color}/10) border border-base! rounded`],
   ],
