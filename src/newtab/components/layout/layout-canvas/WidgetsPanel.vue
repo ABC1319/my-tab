190ce91e6afea505857675b963155e0b22f6a4ec
pinky-pig <2545589993@qq.com>
2024-01-16 18:13:01 +0800

    feat: 弹出组件库

--- src/newtab/components/layout/layout-canvas/WidgetsPanel.vue
+++ src/newtab/components/layout/layout-canvas/WidgetsPanel.vue
@@ -50,23 +50,36 @@ function handleOpenPopup() {
   })
 }
 
+function handleSetPopupTop() {
+  browser.windows.getAll().then((windows) => {
+    const popup = windows.find(window => window.id == widgetsPopupWindowId.value)
+    if (popup && popup.id) {
+      // 说明已经打开了popup窗口，直接激活
+      browser.windows.update(popup.id, {
+        focused: true,
+      })
+    }
+  })
+}
 // -------------------------------弹出 end--------------------------------------//
 </script>
 
 <template>
   <div
     class="
+      widgets-panel-canvas
       absolute top-0px right-8px z-999
-      w-200px h-screen
+      w-fit h-screen
       text-white
       flex-shrink-0
       flex justify-center items-center
     "
   >
+    <!-- panel -->
     <div
       v-show="widgetsPopupWindowId === -1"
       class="
-        w-full h-[calc(100vh_-_100px)]
+        w-200px h-[calc(100vh_-_100px)]
         bg-[#252835]
         rounded-10px py-10px px-10px
         flex flex-col gap-10px flex-shrink-0
@@ -150,25 +163,47 @@ function handleOpenPopup() {
     <!-- toggle button -->
     <div
       v-show="widgetsPopupWindowId !== -1"
+      class="w-fit h-fit p-5"
     >
       <div class="relative">
         <div
           class="
-            flex flex-row justify-center items-center gap-10px
-            h-40px w-full text-12px
+            flex flex-col justify-center items-center gap-10px
+            h-full w-40px text-12px
             cursor-pointer rounded-8px
             bg-#404459
             border-2 border-transparent
             duration-200 ease-in-out transition-all
             hover:border-#767fa2a1
-            px-10px
+            py-10px tracking-6px mb-20px
             peer
           "
+          @click="() => appIsEditCleanHome = false"
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="M19 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11l5 5v11a2 2 0 0 1-2 2" /><path d="M17 21v-8H7v8M7 3v5h8" /></g></svg>
+          <span style="writing-mode:vertical-lr">退出编辑</span>
+        </div>
+        <div class="button-glowing save-exit-btn peer-hover:opacity-100 opacity-0" />
+      </div>
+      <div class="relative">
+        <div
+          class="
+            flex flex-col justify-center items-center gap-10px
+            h-full w-40px text-12px
+            cursor-pointer rounded-8px
+            bg-#404459
+            border-2 border-transparent
+            duration-200 ease-in-out transition-all
+            hover:border-#767fa2a1
+            py-10px tracking-6px
+            peer
+          "
+          @click="handleSetPopupTop"
         >
           <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><rect width="20" height="16" x="2" y="4" rx="2" /><path d="M10 4v4M2 8h20M6 4v4" /></g></svg>
-          <span>置顶 Popup</span>
+          <span style="writing-mode:vertical-lr">找到弹窗</span>
         </div>
-        <div class="peer-hover:opacity-100 button-glowing opacity-0" />
+        <div class="button-glowing find-popup-btn peer-hover:opacity-100 opacity-0" />
       </div>
     </div>
   </div>
@@ -186,8 +221,13 @@ function handleOpenPopup() {
   pointer-events: none;
   z-index: -1;
   border-radius: 9999px;
-  background-image: linear-gradient(45deg, #5021ff, #36e4da, #0047e1);
   filter: blur(60px);
+}
+.save-exit-btn {
+  background-image: linear-gradient(45deg, #1b6ef3, #484f63, #5021ff);
+}
+.find-popup-btn {
+  background-image: linear-gradient(45deg, #5021ff, #36e4da, #0047e1);
 }
 ::-webkit-scrollbar {
   width: 4px;
