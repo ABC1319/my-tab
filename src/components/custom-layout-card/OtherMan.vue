f5f03c82cf51a6f0a34f31071efa5158abf59ef3
pinky-pig <2545589993@qq.com>
2024-01-17 17:10:20 +0800

    feat: add layout demo

---
+++ src/components/custom-layout-card/OtherMan.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div
+    class="select-none w-200px h-200px pointer-events-none"
+  >
+    <img src="/assets/other-images/run_man.svg" alt="">
+  </div>
+</template>
