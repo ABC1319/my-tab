83bdaa7fa6fd456ee9367a3870cc9f5042a423a1
pinky-pig <2545589993@qq.com>
2024-02-08 01:46:02 +0800

    fix: sortable item

--- src/components/flat-sortable/FlatSortableItem.vue
+++ src/components/flat-sortable/FlatSortableItem.vue
@@ -1,12 +1,19 @@
 <script setup lang="ts">
 import type { Ref } from 'vue'
-import { inject } from 'vue'
+import { inject, watch } from 'vue'
 import type { FlatSortableItemEmits, FlatSortableItemProps } from './types'
 
 const props = defineProps<FlatSortableItemProps & { class?: string, inset?: boolean }>()
 const emits = defineEmits<FlatSortableItemEmits>()
 
 const isDragging = inject('isDragging') as Ref<boolean>
+
+watch(isDragging, (v) => {
+  const element = document.querySelector('.flat-sortable-item') as HTMLElement
+  Array.from(element.children).forEach((child) => {
+    (child as HTMLElement).style.pointerEvents = v ? 'none' : 'auto'
+  })
+})
 </script>
 
 <template>
@@ -15,13 +22,7 @@ const isDragging = inject('isDragging') as Ref<boolean>
     :class="props.class"
     :draggable="true"
     style="will-change: transform;"
-    :style="props.style"
   >
-    <div
-      style="width: 100%; height: 100%;"
-      :style="{ 'pointer-events': isDragging ? 'none' : 'auto' }"
-    >
-      <slot />
-    </div>
+    <slot />
   </div>
 </template>
