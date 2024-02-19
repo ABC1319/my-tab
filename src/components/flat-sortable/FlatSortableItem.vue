a21d0b3ab7ad042d7ff34ac3717187b37e5c2fbd
pinky-pig <2545589993@qq.com>
2024-02-19 11:46:54 +0800

    fix: updated flat-sortable

--- src/components/flat-sortable/FlatSortableItem.vue
+++ src/components/flat-sortable/FlatSortableItem.vue
@@ -1,21 +1,8 @@
 <script setup lang="ts">
-import type { Ref } from 'vue'
-import { inject, watch } from 'vue'
 import type { FlatSortableItemEmits, FlatSortableItemProps } from './types'
 
 const props = defineProps<FlatSortableItemProps & { class?: string, inset?: boolean }>()
 const emits = defineEmits<FlatSortableItemEmits>()
-
-const isDragging = inject('isDragging') as Ref<boolean>
-
-watch(isDragging, (v) => {
-  const items = document.querySelectorAll('.flat-sortable-item') as NodeListOf<HTMLElement>
-  items.forEach((element) => {
-    Array.from(element.children).forEach((child) => {
-      (child as HTMLElement).style.pointerEvents = v ? 'none' : 'auto'
-    })
-  })
-})
 </script>
 
 <template>
